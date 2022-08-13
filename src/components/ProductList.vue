0de374720dd3df8b8250d92c786667634a28abd4
kshitij <kmgaikwad97@gmail.com>
2022-08-13 15:40:09 +0530

    api completed

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="d-flex align-items-stretch flex-wrap">
+   <ProductCard v-for="product in products" :key="product.id" :product="product"/>
+   <!-- <ProductCard/>
    <ProductCard/>
    <ProductCard/>
    <ProductCard/>
    <ProductCard/>
    <ProductCard/>
-   <ProductCard/>
-   <ProductCard/>
-   <ProductCard/>
+   <ProductCard/> -->
 </div>
 </template>
 
@@ -16,6 +16,14 @@ import ProductCard from './ProductCard.vue'
 export default {
     components:{
         ProductCard
+    },
+    computed:{
+        products(){
+            return this.$store.state.products;
+        }
+    },
+    mounted(){
+        this.$store.dispatch('getProducts');
     }
 };
 </script>
